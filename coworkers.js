d0ddcdf2986a59f6d315911d1f478cc8923ab70b
Johan Sellström <johan.sellstrom@combient.com>
2017-04-23 08:14:04 +0200

    coworkers page with no edu

---
+++ coworkers.js
@@ -0,0 +1,214 @@
+$(function(){ // on dom ready
+
+// photos from flickr with creative commons license
+
+var cy = cytoscape({
+  container: document.getElementById('cy'),
+
+  boxSelectionEnabled: false,
+  autounselectify: true,
+
+  style: cytoscape.stylesheet()
+    .selector('node')
+      .css({
+        'height': 80,
+        'width': 80,
+        'background-fit': 'cover',
+        'border-color': '#000',
+        'border-width': 3,
+        'border-opacity': 0.5
+      })
+    .selector('.eating')
+      .css({
+        'border-color': 'red'
+      })
+    .selector('.eater')
+      .css({
+        'border-width': 9
+      })
+    .selector('edge')
+      .css({
+        'width': 6,
+        'target-arrow-shape': 'triangle',
+        'line-color': '#ffaaaa',
+        'target-arrow-color': '#ffaaaa',
+        'curve-style': 'bezier'
+      })
+    .selector('#hollieann')
+        .css({
+          'background-image': 'http://carechain.io/nodes/hollieann.png'
+      })
+    .selector('#combient')
+        .css({
+          'background-image': 'http://carechain.io/nodes/combient.png'
+      })
+    .selector('#mycube')
+      .css({
+        'background-image': 'http://carechain.io/nodes/mycube.png'
+      })
+  .selector('#johan')
+      .css({
+        'background-image': 'http://carechain.io/nodes/johan.png'
+      })
+  .selector('#salientpoint')
+      .css({
+        'background-image': 'http://carechain.io/nodes/salientpoint.png'
+      })
+  .selector('#next')
+      .css({
+        'background-image': 'http://carechain.io/nodes/next.png'
+      })
+  .selector('#provivus')
+      .css({
+        'background-image': 'http://carechain.io/nodes/provivus.png'
+      })
+  .selector('#ericsson')
+      .css({
+            'background-image': 'http://carechain.io/nodes/ericsson.png'
+      })
+  .selector('#eniro')
+      .css({
+            'background-image': 'http://carechain.io/nodes/eniro.png'
+      })
+  .selector('#saab')
+      .css({
+        'background-image': 'http://carechain.io/nodes/saab.png'
+      })
+  .selector('#oracle')
+      .css({
+        'background-image': 'http://carechain.io/nodes/oracle.png'
+      })
+  .selector('#torsten')
+      .css({
+        'background-image': 'http://carechain.io/nodes/torsten.png'
+      })
+  .selector('#theresa')
+      .css({
+        'background-image': 'http://carechain.io/nodes/theresa.png'
+      })
+  .selector('#head')
+      .css({
+        'background-image': 'http://carechain.io/nodes/head.png'
+      })
+  .selector('#anders')
+      .css({
+        'background-image': 'http://carechain.io/nodes/anders.png'
+      })
+  .selector('#advatar')
+      .css({
+        'background-image': 'http://carechain.io/nodes/advatar.png'
+      })
+  .selector('#icon')
+      .css({
+        'background-image': 'http://carechain.io/nodes/icon.png'
+      }),
+
+  elements: {
+    nodes: [
+      { data: { id: 'hollieann' } },
+      { data: { id: 'combient' } },
+      { data: { id: 'mycube' } },
+      { data: { id: 'johan' } },
+      { data: { id: 'salientpoint' } },
+      { data: { id: 'next' } },
+      { data: { id: 'ericsson' } },
+      { data: { id: 'saab' } },
+      { data: { id: 'oracle' } },
+      { data: { id: 'torsten' } },
+      { data: { id: 'theresa' } },
+      { data: { id: 'eniro' } },
+      { data: { id: 'head' } },
+      { data: { id: 'anders' } },
+      { data: { id: 'icon' } },
+      { data: { id: 'advatar' } },
+      { data: { id: 'provivus' } }
+    ],
+    edges: [
+      { data: { source: 'hollieann', target: 'icon' } },
+      { data: { source: 'hollieann', target: 'advatar' } },
+      { data: { source: 'hollieann', target: 'provivus' } },
+      { data: { source: 'johan', target: 'head' } },
+      { data: { source: 'johan', target: 'icon' } },
+      { data: { source: 'johan', target: 'advatar' } },
+      { data: { source: 'johan', target: 'mycube' } },
+      { data: { source: 'johan', target: 'saab' } },
+      { data: { source: 'johan', target: 'ericsson' } },
+      { data: { source: 'johan', target: 'combient' } },
+      { data: { source: 'johan', target: 'provivus' } },
+      { data: { source: 'torsten', target: 'next' } },
+      { data: { source: 'torsten', target: 'eniro' } },
+      { data: { source: 'torsten', target: 'oracle' } },
+      { data: { source: 'torsten', target: 'salientpoint' } },
+      { data: { source: 'torsten', target: 'ericsson' } },
+      { data: { source: 'torsten', target: 'combient' } },
+      { data: { source: 'torsten', target: 'provivus' } },
+      { data: { source: 'theresa', target: 'ericsson' } },
+      { data: { source: 'theresa', target: 'salientpoint' } },
+      { data: { source: 'theresa', target: 'saab' } },
+      { data: { source: 'theresa', target: 'provivus' } },
+      { data: { source: 'anders', target: 'ericsson' } },
+      { data: { source: 'anders', target: 'eniro' } },
+      { data: { source: 'anders', target: 'combient' } },
+      { data: { source: 'anders', target: 'provivus' } }
+    ]
+  },
+
+  layout: {
+    name: 'breadthfirst',
+    directed: false,
+    padding: 10
+  }
+}); // cy init
+
+cy.on('tap', 'node', function( e ){
+  var nodes = e.target;
+  var tapped = nodes;
+  var food = [];
+
+  nodes.addClass('eater');
+
+  for(;;){
+    var connectedEdges = nodes.connectedEdges(function(el){
+      return !el.target().anySame( nodes );
+    });
+
+    var connectedNodes = connectedEdges.targets();
+
+    Array.prototype.push.apply( food, connectedNodes );
+
+    nodes = connectedNodes;
+
+    if( nodes.empty() ){ break; }
+  }
+
+  var delay = 0;
+  var duration = 500;
+  for( var i = food.length - 1; i >= 0; i-- ){ (function(){
+    var thisFood = food[i];
+    var eater = thisFood.connectedEdges(function(el){
+      return el.target().same(thisFood);
+    }).source();
+
+    thisFood.delay( delay, function(){
+      eater.addClass('eating');
+    } ).animate({
+      position: eater.position(),
+      css: {
+        'width': 10,
+        'height': 10,
+        'border-width': 0,
+        'opacity': 0
+      }
+    }, {
+      duration: duration,
+      complete: function(){
+        thisFood.remove();
+      }
+    });
+
+    delay += duration;
+  })(); } // for
+
+}); // on tap
+
+}); // on dom ready
